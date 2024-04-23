e0e3e91b3c43ea7e0864bb44adc67701df4a87d8
hurui <xunyihu@163.com>
2024-04-23 18:21:07 +0800

    线性表-顺序存储

--- C/01-Linear table/example 2-1.c
+++ C/01-Linear table/example 2-1.c
@@ -17,3 +17,49 @@
     5）定位 Locate(L, x)。查找线性表中数据元素值等于 x 的结点序号，若有多个数据元素值与 x 相等，运算结果为这些结点序号中的最小值，若找不到该结点，则运算结果为 0 。
     6）删除 Delete(L, i)。
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ *  顺序存储实现
+*/
+
+const int MaxSize = 100;    //预先定义数组大小
+
+//定义结点的类型
+typedef struct 
+{
+    int num;        //学号
+    char name[8];   //姓名
+    char sex[2];    //性别
+    int age;        //年龄
+    int score;      //入学成绩
+} DataType;         
+
+//顺序表的类型
+typedef struct {
+    DataType data[MaxSize];     //存放数据的数组
+    int length;                 //线性表的实际长度
+} SeqList;
+
+//插入运算
+void InsertSeqlist(SeqList L, DataType x, int i)
+{
+    if (L.length == MaxSize)    exit("表已满");
+    if (i < 1 || i > L.length)  exit("位置错");
+    for (int j = L.length; j >= i; j--) 
+        L.data[j] = L.data[j-1];    //依次后移一位
+    L.data[i - 1] = x;
+    L.length++;                     //表长度加 1
+}
+
+//删除
+void DeleteSeqlist(SeqList L, int i)
+{
+    if (i < 1 || i > L.length)  exit("位置错");
+    for (int j = i; j < L.length; j++)
+        L.data[j] = L.data[j+1];        //依次前移一位
+    L.length--;                         //表长度减 1
+}
